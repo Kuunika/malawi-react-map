a19c95dc0adf3cbd51ee055fdae8b5c9c7ac5b97
kapsata <mkapsata@gmail.com>
2019-02-09 10:39:23 +0200

    Refactored Path component

--- src/components/Path.js
+++ src/components/Path.js
@@ -1,21 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 
-//TODO: Functional Stateless Component
-export default class Path extends Component {
-  render() {
-    const { name, boundary } = this.props.selectedDistricts;
-    const isHiglighted = district => this.props.districts.includes(district);
-    return (
-      <path
-        data-for="svgTooltip"
-        fill={isHiglighted(name) ? this.props.selectedColor : "inherit"}
-        onClick={this.props.onClick}
-        data-tip={name}
-        d={boundary}
-        title={name}
-        xlinkTitle={name}
-        id={name}
-      />
-    );
-  }
-}
+const Path = props => {
+  const { selectedColor, district, onClick, selectedDistricts } = props;
+  const { name, boundary } = district;
+
+  const isHiglighted = district => selectedDistricts.includes(district);
+
+  return (
+    <path
+      data-for="svgTooltip"
+      fill={isHiglighted(name) ? selectedColor : "inherit"}
+      onClick={onClick}
+      data-tip={name}
+      d={boundary}
+      title={name}
+      xlinkTitle={name}
+      id={name}
+    />
+  );
+};
+export default Path;
